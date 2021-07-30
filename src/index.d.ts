347555a5a7a1ee293ddfa583b670e8d3ffcf932e
Evan <Evancat77@gmail.com>
2021-07-30 17:12:46 -0400

    woo make it actually useable

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,11 +1,7 @@
-import LinkedList from "LinkedList";
-import MaxPriorityQueue from "PriorityQueues/MaxPriorityQueue";
-import MinPriorityQueue from "PriorityQueues/MinPriorityQueue";
-import Queue from "Queue";
-import Stack from "Stack";
+import LinkedList from "./LinkedList";
+import MaxPriorityQueue from "./PriorityQueues/MaxPriorityQueue";
+import MinPriorityQueue from "./PriorityQueues/MinPriorityQueue";
+import Queue from "./Queue";
+import Stack from "./Stack";
 
-export { LinkedList };
-export { MinPriorityQueue };
-export { MaxPriorityQueue };
-export { Queue };
-export { Stack };
+export { LinkedList, MinPriorityQueue, MaxPriorityQueue, Queue, Stack };
