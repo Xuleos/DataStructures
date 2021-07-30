779ba750648f62beb293cbd71f9773fd0a817c56
Evan <Evancat77@gmail.com>
2021-07-30 16:56:46 -0400

    actual main file lol

---
+++ src/index.d.ts
@@ -0,0 +1,11 @@
+import LinkedList from "LinkedList";
+import MaxPriorityQueue from "PriorityQueues/MaxPriorityQueue";
+import MinPriorityQueue from "PriorityQueues/MinPriorityQueue";
+import Queue from "Queue";
+import Stack from "Stack";
+
+export { LinkedList };
+export { MinPriorityQueue };
+export { MaxPriorityQueue };
+export { Queue };
+export { Stack };
---
+++ src/init.lua
@@ -0,0 +1,7 @@
+return {
+    LinkedList = require(script.LinkedList),
+    MaxPriorityQueue = require(script.PriorityQueues.MaxPriorityQueue),
+    MinPriorityQueue = require(script.PriorityQueues.MinPriorityQueue),
+    Queue = require(script.Queue),
+    Stack = require(script.Stack)
+}
